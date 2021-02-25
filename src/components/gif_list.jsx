ed4c09f5ced139de19748dcf0378d1a38e14b18b
Anton <panteleev23@gmail.com>
2021-02-25 14:17:47 +1100

    gif-list implemented, and refactored

--- src/components/gif_list.jsx
+++ src/components/gif_list.jsx
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 import Gif from './gif.jsx';
 
 export default class GifList extends Component {
-  render() {
-
+  renderList = () => {
     return this.props.gifs.map(gif => {
       return <Gif id={gif.id} key={gif.id} />
-    });
+    })
+  }
+
+  render() {
+    return (
+      <div className="gif-list">
+        {this.renderList()}
+      </div>
+    )
   }
 }
