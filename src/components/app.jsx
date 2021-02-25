51fed739391dca11deac32eb94c8c8627c10336c
Anton <panteleev23@gmail.com>
2021-02-25 14:30:03 +1100

    searchbar listener

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,22 +3,31 @@ import SearchBar from './search_bar.jsx'
 import Gif from './gif.jsx'
 import GifList from './gif_list.jsx'
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      gifs: [],
+      selectedGifID: "6mkIIJIQvfgzny5USS"
+    }
+  }
+
   render() {
-    const gifs = [
-      { id: "29I5gneghwWbCczhbb" },
-      { id: "yfhp7az2ZBH0I" }
-    ];
+    // const gifs = [
+    //   { id: "29I5gneghwWbCczhbb" },
+    //   { id: "yfhp7az2ZBH0I" }
+    // ];
 
     return (
       <div>
         <div className="left-scene">
           <SearchBar />
           <div className="selected-gif">
-            <Gif id="6mkIIJIQvfgzny5USS" />
+            <Gif id={this.state.selectedGifID} />
           </div>
         </div>
         <div className="right-scene">
-          <GifList gifs={gifs} />
+          <GifList gifs={this.state.gifs} />
         </div>
       </div>
     );
