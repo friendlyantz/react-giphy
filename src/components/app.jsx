d3afc92a475a0ecb915265f8ead2e6ff38bb52ef
Anton <panteleev23@gmail.com>
2021-02-25 15:17:25 +1100

    giphy api callback function implemented. api method, oldschool, no SDK

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import giphy from "giphy-api";
+
 import SearchBar from './search_bar.jsx'
 import Gif from './gif.jsx'
 import GifList from './gif_list.jsx'
@@ -10,6 +12,23 @@ class App extends Component {
       gifs: [],
       selectedGifID: "6mkIIJIQvfgzny5USS"
     }
+
+    this.search("lala");
+  }
+
+  search = (query) => {
+    // apicall
+    giphy('nbZ987awZlAD6T6AMZAi1cohEZ32R17e').search({
+      q: query,
+      rating: 'g',
+      limit: 10
+    }, (error, res) => { // removed function in liue to arrow function!!!!! in order it to keep in callback
+      // console.log(error)
+      console.log(res.data)
+      this.setState({
+        gifs: res.data
+      });
+    });
   }
 
   render() {
